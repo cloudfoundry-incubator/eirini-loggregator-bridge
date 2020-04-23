b1c8bd7323c357c09ab7fbf1692532a96c0cc6a3
Ettore Di Giacinto <edigiacinto@suse.com>
2020-04-23 14:55:14 +0200

    fix: use yaml annotation for config
    WIP: it's not working yet

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
-	"reflect"
 	"strings"
 
 	eirinix "github.com/SUSE/eirinix"
+	"gopkg.in/yaml.v2"
 
 	configpkg "github.com/SUSE/eirini-loggregator-bridge/config"
 	. "github.com/SUSE/eirini-loggregator-bridge/logger"
@@ -18,7 +19,16 @@ import (
 var cfgFile string
 var kubeconfig string
 
-var config configpkg.ConfigType
+// See: https://github.com/spf13/viper/issues/188
+// Viper, without default config doesn't set keys, and later
+// they are not populated into config when unmarshalling
+var config configpkg.ConfigType = configpkg.ConfigType{
+	Namespace:           "default",
+	LoggregatorEndpoint: "loggregator-endpoint",
+	LoggregatorCAPath:   "loggregator-ca-path",
+	LoggregatorCertPath: "loggregator-cert-path",
+	LoggregatorKeyPath:  "loggregator-key-path",
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "eirini-loggregator-bridge",
@@ -66,31 +76,21 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&kubeconfig, "kubeconfig", "", "kubeconfig file path. This is optional, in cluster config will be used if not set")
 }
 
-// See: https://github.com/spf13/viper/issues/188#issuecomment-399884438
-func BindEnvs(iface interface{}, parts ...string) {
-	ifv := reflect.ValueOf(iface)
-	ift := reflect.TypeOf(iface)
-	for i := 0; i < ift.NumField(); i++ {
-		v := ifv.Field(i)
-		t := ift.Field(i)
-		tv, ok := t.Tag.Lookup("mapstructure")
-		if !ok {
-			continue
-		}
-		switch v.Kind() {
-		case reflect.Struct:
-			BindEnvs(v.Interface(), append(parts, tv)...)
-		default:
-			viper.BindEnv(strings.Join(append(parts, tv), "."))
-		}
-	}
-}
-
 func initConfig() {
 	if cfgFile != "" {
+		viper.SetConfigType("yaml")
+		emptyConfigBytes, err := yaml.Marshal(config)
+		if err != nil {
+			LogError("Can't marshal config:", err.Error())
+			os.Exit(1)
+		}
+
+		emptyConfigReader := bytes.NewReader(emptyConfigBytes)
+		viper.MergeConfig(emptyConfigReader)
+
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
-		if err := viper.ReadInConfig(); err != nil {
+		if err := viper.MergeInConfig(); err != nil {
 			LogError("Can't read config:", err.Error())
 			os.Exit(1)
 		}
@@ -98,8 +98,6 @@ func initConfig() {
 
 	viper.AutomaticEnv() // read in environment variables that match
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-
-	BindEnvs(config)
 	viper.Unmarshal(&config)
 
 	LogDebug("Namespace: ", fmt.Sprintf("%s", config.Namespace))
