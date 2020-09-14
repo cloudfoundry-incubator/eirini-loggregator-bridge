2cae529676cd5fc075a5c98c8da971e9726ac1a5
Ettore Di Giacinto <edigiacinto@suse.com>
2020-09-14 14:51:07 +0200

    cover all cases with unit tests

--- podwatcher/extension_test.go
+++ podwatcher/extension_test.go
@@ -22,13 +22,27 @@ func decodePatches(resp admission.Response) string {
 	return r
 }
 
+func jsonifyPatches(resp admission.Response) []string {
+	var r []string
+	for _, patch := range resp.Patches {
+		r = append(r, patch.Json())
+	}
+	return r
+}
+
 const (
-	addOpiPatch = `{"op":"add","path":"/spec/containers/0/command","value":["dumb-init","--","/bin/sh","-c","(  /lifecycle/launch \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	addOpiPatch                = `{"op":"add","path":"/spec/containers/0/command","value":["dumb-init","--","/bin/sh","-c","(  /lifecycle/launch \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	addUploaderPatch           = `{"op":"add","path":"/spec/containers/0/command","value":["/bin/sh","-c","( /packs/uploader \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	addDownloaderPatch         = `{"op":"add","path":"/spec/initContainers/0/command","value":["/bin/sh","-c","( /packs/downloader \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	addExecutorPatch           = `{"op":"add","path":"/spec/initContainers/0/command","value":["/bin/sh","-c","( /packs/executor \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	stagingFullPatchUploader   = `{"op":"add","path":"/spec/containers/0/command","value":["/bin/sh","-c","( /packs/uploader \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	stagingFullPatchExecutor   = `{"op":"add","path":"/spec/initContainers/0/command","value":["/bin/sh","-c","( /packs/executor \u0026\u0026 sleep 5 ) || sleep 5"]}`
+	stagingFullPatchDownloader = `{"op":"add","path":"/spec/initContainers/1/command","value":["/bin/sh","-c","( /packs/downloader \u0026\u0026 sleep 5 ) || sleep 5"]}`
 )
 
 var _ = Describe("Eirini extension", func() {
 	eirinixcat := eirinixcatalog.NewCatalog()
-	gracefulInjector := NewgracePeriodInjector(&GraceOptions{})
+	gracefulInjector := NewGracePeriodInjector(&GraceOptions{})
 	eiriniManager := eirinixcat.SimpleManager()
 	request := admission.Request{}
 	pod := &corev1.Pod{
@@ -41,10 +55,9 @@ var _ = Describe("Eirini extension", func() {
 		},
 	}
 
-	BeforeEach(func() {
-		gracefulInjector = NewgracePeriodInjector(&GraceOptions{})
+	JustBeforeEach(func() {
+		gracefulInjector = NewGracePeriodInjector(&GraceOptions{})
 		eirinixcat = eirinixcatalog.NewCatalog()
-		gracefulInjector = NewgracePeriodInjector(&GraceOptions{})
 		eiriniManager = eirinixcat.SimpleManager()
 
 		raw, err := json.Marshal(pod)
@@ -65,15 +78,157 @@ var _ = Describe("Eirini extension", func() {
 				Expect(gracefulInjector.Options.StagingExecutorEntrypoint).To(Equal("/packs/executor"))
 				Expect(gracefulInjector.Options.RuntimeEntrypoint).To(Equal("/lifecycle/launch"))
 			})
-		})
-	})
 
-	Describe("GracePeriod Injector", func() {
-		Context("when handling a runtime app", func() {
-			It("Injects a grace period", func() {
-				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(addOpiPatch))
+			It("sets config", func() {
+				gracefulInjector = NewGracePeriodInjector(&GraceOptions{
+					FailGracePeriod:             "12",
+					SuccessGracePeriod:          "90",
+					StagingDownloaderEntrypoint: "foo",
+					StagingUploaderEntrypoint:   "bar",
+					StagingExecutorEntrypoint:   "baz",
+					RuntimeEntrypoint:           "42",
+				})
+
+				Expect(gracefulInjector.Options.FailGracePeriod).To(Equal("12"))
+				Expect(gracefulInjector.Options.SuccessGracePeriod).To(Equal("90"))
+				Expect(gracefulInjector.Options.StagingDownloaderEntrypoint).To(Equal("foo"))
+				Expect(gracefulInjector.Options.StagingUploaderEntrypoint).To(Equal("bar"))
+				Expect(gracefulInjector.Options.StagingExecutorEntrypoint).To(Equal("baz"))
+				Expect(gracefulInjector.Options.RuntimeEntrypoint).To(Equal("42"))
 			})
 		})
 	})
 
+	Describe("GracePeriod Injector", func() {
+		Context("when handling a Eirini runtime app", func() {
+			It("Injects a grace period", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(addOpiPatch))
+			})
+		})
+
+		Context("when a non-opi pod is handled", func() {
+			BeforeEach(func() {
+				pod = &corev1.Pod{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name: "foo",
+							},
+						},
+					},
+				}
+			})
+			It("Does not inject a grace period", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(""))
+			})
+		})
+
+		Context("when a staging pod is handled", func() {
+			BeforeEach(func() {
+				pod = &corev1.Pod{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name: "opi-task-uploader",
+							},
+						},
+					},
+				}
+			})
+			It("Does inject a grace period if the container is opi-task-uploader", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(addUploaderPatch))
+			})
+		})
+
+		Context("when a pod is handled", func() {
+			BeforeEach(func() {
+				pod = &corev1.Pod{
+					Spec: corev1.PodSpec{
+						InitContainers: []corev1.Container{
+							{
+								Name: "opi-task-uploader",
+							},
+						},
+					},
+				}
+			})
+			It("Does not inject a grace period if the initcontainer doesn't have the correct name", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(""))
+			})
+		})
+
+		Context("when a pod is handled", func() {
+			BeforeEach(func() {
+				pod = &corev1.Pod{
+					Spec: corev1.PodSpec{
+						InitContainers: []corev1.Container{
+							{
+								Name: "opi-task-downloader",
+							},
+						},
+					},
+				}
+			})
+			It("Does inject a grace period if the initcontainer have the opi-task-downloader name", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(addDownloaderPatch))
+			})
+		})
+
+		Context("when a staging pod is handled", func() {
+			BeforeEach(func() {
+				pod = &corev1.Pod{
+					Spec: corev1.PodSpec{
+						InitContainers: []corev1.Container{
+							{
+								Name: "opi-task-executor",
+							},
+						},
+					},
+				}
+			})
+			It("Does inject a grace period if the initcontainer if have the opi-task-executor name", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(addExecutorPatch))
+			})
+		})
+
+		Context("when a nil pod is passed by", func() {
+			BeforeEach(func() {
+				pod = nil
+			})
+
+			It("Doesn't return any patch", func() {
+				Expect(decodePatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))).To(Equal(""))
+			})
+		})
+
+		Context("when a (full) staging pod is handled", func() {
+			BeforeEach(func() {
+				pod = &corev1.Pod{
+					Spec: corev1.PodSpec{
+						InitContainers: []corev1.Container{
+							{
+								Name: "opi-task-executor",
+							},
+							{
+								Name: "opi-task-downloader",
+							},
+						},
+						Containers: []corev1.Container{
+							{
+								Name: "opi-task-uploader",
+							},
+						},
+					},
+				}
+			})
+
+			It("Does inject a grace period in all Containers and InitContainers", func() {
+				patches := jsonifyPatches(gracefulInjector.Handle(context.TODO(), eiriniManager, pod, request))
+				Expect(patches).To(ContainElement(stagingFullPatchExecutor))
+				Expect(patches).To(ContainElement(stagingFullPatchUploader))
+				Expect(patches).To(ContainElement(stagingFullPatchDownloader))
+				Expect(len(patches)).To(Equal(3))
+			})
+		})
+	})
 })
